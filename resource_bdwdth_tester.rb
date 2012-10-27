30ce239712380276cb8da3bfeb940ab36ab33326
Joshua Burkhart <burkhart.joshua@gmail.com>
2012-10-27 15:38:02 -0700

    resource bandwith tester working well...

--- resource_bdwdth_tester.rb
+++ resource_bdwdth_tester.rb
@@ -2,7 +2,7 @@
 
 #Usage: ruby resource_bdwdth_tester.rb <# of times to request each resource> <URI filename>
 
-#Example:
+#Example: ruby resource_bdwdth_tester.rb 2 test_uri.txt
 
 class DataRow
     attr_accessor :local_node_name
@@ -21,21 +21,58 @@ class DataRow
     attr_accessor :uri
 
     def to_s
-        print "#{@local_node_name},"
-        print "#{@local_ip_addr},"
-        print "#{@local_mac_addr},"
-        print "#{@local_geoip_info},"
-
-        print "#{@remote_node_name},"
-        print "#{@remote_ip_addr},"
-        print "#{@remote_mac_addr},"
-        print "#{@remote_geoip_info},"
-
-        print "#{@timestamp},"
-        print "#{@bandwidth},"
-        print "#{@rtt},"
-        print "#{@uri}\n"
+        return\
+        "#{@local_node_name},"\
+        "#{@local_ip_addr},"\
+        "#{@local_mac_addr},"\
+        "#{@local_geoip_info},"\
+        \
+        "#{@remote_node_name},"\
+        "#{@remote_ip_addr},"\
+        "#{@remote_mac_addr},"\
+        "#{@remote_geoip_info},"\
+        \
+        "#{@timestamp},"\
+        "#{@bandwidth},"\
+        "#{@rtt},"\
+        "#{@uri}"
     end
+end
+
+def findLocalHostname()
+    hostname = %x(hostname -f)
+    return hostname.strip
+end
+
+def findLocalIp()
+    ip = %x(curl -s http://automation.whatismyip.com/n09230945.asp)
+    return ip.strip
+end
+
+def findLocalMac()
+    en_out = %x(ifconfig -a)
+    en_out.match(/ether (\w{2}:\w{2}:\w{2}:\w{2})|HWaddr (\w{2}:\w{2}:\w{2}:\w{2})/)
+    mac = $1
+    return mac.strip
+end
+
+def findRemoteHostname(resource)
+    resource.strip.match(/http:\/\/([a-z0-9.]+[a-z0-9])\//)
+    fqdn = $1
+    if(fqdn.nil?) #assume resource is an ip address...
+        host_out = %x(host #{resource.strip})
+        host_out.match(/pointer (.*$)/)
+        fqdn = $1
+    end
+    return fqdn.strip
+end
+
+def findRemoteIp(resource)
+    hostname = findRemoteHostname(resource)
+    nslookup_out = %x(nslookup #{hostname})
+    nslookup_out.match(/Address: (\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})/)
+    ip = $1
+    return ip.strip
 end
 
 def measureWgetBdwth(resource)
@@ -43,7 +80,7 @@ def measureWgetBdwth(resource)
     giga = 1000000
     mega = 1000
     kilo = 1 
-    wget_out = %x(wget #{resource} 2>/dev/null)
+    wget_out = %x(wget #{resource.strip} -O /dev/null 2>&1)
     wget_out.match(/\((\d+.?\d* [A-Z]B\/s)\)/)
     speed = $1
     if(speed.match(/(\d+.?\d*) TB\/s/))
@@ -69,44 +106,49 @@ end
 request_count = Integer(ARGV[0])
 uri_filename = ARGV[1]
 
-UNSET="empty"
-ERROR="unavailable"
+UNSET="<empty>"
+ERROR="<unavailable>"
 
 table = Array.new
-uri_file_handle = File.open(uri_filename,'r')
+out_file_handle = File.open("resource_bdwdth_output.log",'a')
 
 print "working..."
-while (uri = uri_file_handle.gets) 
-    request_count.times {
-        print "."
-        STDOUT.flush
-        row = DataRow.new
+while true
+    uri_file_handle = File.open(uri_filename,'r')
+    while (uri = uri_file_handle.gets) 
+        request_count.times {
+            print "."
+            STDOUT.flush
+            row = DataRow.new
 
-        row.local_node_name = UNSET
-        row.local_ip_addr = UNSET
-        row.local_mac_addr = UNSET
-        row.local_geoip_info = UNSET
+            row.local_node_name = findLocalHostname()
+            row.local_ip_addr = findLocalIp()
+            row.local_mac_addr = findLocalMac()
+            row.local_geoip_info = UNSET
 
-        row.remote_node_name = UNSET
-        row.remote_ip_addr = UNSET
-        row.remote_mac_addr = UNSET
-        row.remote_geoip_info = UNSET
+            row.remote_node_name = findRemoteHostname(uri)
+            row.remote_ip_addr = findRemoteIp(uri)
+            row.remote_mac_addr = UNSET
+            row.remote_geoip_info = UNSET
 
-        row.timestamp = UNSET
-        begin
-            row.bandwidth = measureWgetBdwth(uri)
-        rescue
-            puts "Exception Raised: #{$!}"
-            row.bandwidth = ERROR
-        end
-        row.rtt = UNSET
-        row.uri = UNSET
-        table << row
+            row.timestamp = Time.now
+            begin
+                row.bandwidth = measureWgetBdwth(uri)
+            rescue
+                puts "Exception Raised: #{$!}"
+                row.bandwidth = ERROR
+            end
+            row.rtt = UNSET
+            row.uri = uri.strip
+            table << row
+        }
+    end
+    puts
+    table.each { |row|
+        out_file_handle.puts row.to_s
     }
+    uri_file_handle.close
+    sleep 1
 end
-uri_file_handle.close
-puts
-table.each { |row|
-    row.to_s
-}
+out_file_handle.close
 puts "done."
