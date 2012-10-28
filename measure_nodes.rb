d8e597cdfa1e80016c499ff019392dce3a8e5d57
Joshua Burkhart <burkhart.joshua@gmail.com>
2012-10-27 18:33:53 -0700

    adding alternate source ip feature...

--- measure_nodes.rb
+++ measure_nodes.rb
@@ -1,8 +1,12 @@
 #!/usr/local/bin/ruby
 
-#Usage: ruby measure_nodes.rb <# of times to request each resource> <URI filename>
+h = <<-EOS
+#Usage: ruby measure_nodes.rb <alternate src count> <alternate src type (ingress / egress)> <uri request count> </path/to/uri/list/file>
 
-#Example: ruby measure_nodes.rb 2 test_uri.txt
+#Example: ruby measure_nodes.rb 0 e 2 test_uri.txt
+
+#Note: This program requires the hping3 tool (http://www.hping.org/) be installed and runnable by the user in order to use the alternate source ip feature.
+EOS
 
 class DataRow
     attr_accessor :local_node_name
@@ -56,43 +60,49 @@ def findLocalMac()
     return mac.strip
 end
 
-def findRemoteHostname(resource)
-    resource.strip.match(/http:\/\/([a-z0-9.]+[a-z0-9])\//)
+def findRemoteHostname(uri)
+    uri.strip.match(/http:\/\/([a-z0-9.]+[a-z0-9])\//)
     fqdn = $1
-    if(fqdn.nil?) #assume resource is an ip address...
-        host_out = %x(host #{resource.strip})
+    if(fqdn.nil?) #assume uri is an ip address...
+        host_out = %x(host #{uri.strip})
         host_out.match(/pointer (.*$)/)
         fqdn = $1
     end
     return fqdn.strip
 end
 
-def findRemoteIp(resource)
-    hostname = findRemoteHostname(resource)
+def findRemoteIp(uri)
+    hostname = findRemoteHostname(uri)
     nslookup_out = %x(nslookup #{hostname})
     nslookup_out.match(/Address: (\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})/)
     ip = $1
     return ip.strip
 end
 
-def measurePingRtt(resource)
-    hostname = findRemoteHostname(resource)
-    ping_out = %x(ping -c1 #{hostname})
-    ping_out.match(/time=(\w+.?\w+) ms/)
-    rtt = $1
-    if(!rtt.nil?)
-        return rtt.strip
+def sendAltSrcSyns(alt_src_count,uri,alt_src_type)
+    alt_src_ip = nil
+    hostname = findRemoteHostname(uri)
+    ip = findRemoteIp(uri)
+    ip_ary = ip.split(/\./)
+    if(alt_src_type == "i")
+        #assume CIDR block with 24 bit prefix (a.b.c.d/24)
+        alt_src_ip = "#{ip_ary[0]}.#{ip_ary[1]}.#{ip_ary[2]}.#{rand(255)}"
+    elsif(alt_src_type == "e")
+        alt_src_ip = "#{rand(ip_ary[0])}.#{rand(255)}.#{rand(255)}.#{rand(255)}"
     else
-        raise "UNKNOWN RTT INDICATOR:\n#{ping_out}"
+        alt_src_ip = nil
+    end
+    if(alt_src_count > 0 && !alt_src_ip.nil?)
+        %(sudo hping3 -S -a #{alt_src_ip} #{hostname} -c #{alt_src_count})
     end
 end
 
-def measureWgetBdwth(resource)
+def measureWgetBdwth(uri)
     tera = 1000000000
     giga = 1000000
     mega = 1000
     kilo = 1 
-    wget_out = %x(wget #{resource.strip} -O /dev/null 2>&1)
+    wget_out = %x(wget #{uri.strip} -O /dev/null 2>&1)
     wget_out.match(/\((\d+.?\d* [A-Z]B\/s)\)/)
     speed = $1
     if(speed.match(/(\d+.?\d*) TB\/s/))
@@ -109,13 +119,34 @@ def measureWgetBdwth(resource)
     return ret #in KB/s
 end
 
-if(ARGV.length != 2)
-    puts "THIS SCRIPT REQUIRES TWO ARGUMENTS\n#Usage: ruby resource_bdwdth_tester.rb <# of times to request each resource> <URI filename>"
+def measurePingRtt(uri)
+    hostname = findRemoteHostname(uri)
+    ping_out = %x(ping -c1 #{hostname})
+    ping_out.match(/time=(\w+.?\w+) ms/)
+    rtt = $1
+    if(!rtt.nil?)
+        return rtt.strip
+    else
+        raise "UNKNOWN RTT INDICATOR:\n#{ping_out}"
+    end
+end
+
+if(ARGV.length != 4)
+    puts "THIS SCRIPT REQUIRES 4 ARGUMENTS"
+    puts h
     exit
 end
 
-request_count = Integer(ARGV[0])
-uri_filename = ARGV[1]
+alt_src_count = Integer(ARGV[0])
+alt_src_type = ARGV[1].to_s[0,1]
+request_count = Integer(ARGV[2])
+uri_filename = ARGV[3]
+
+puts "Using alternate source #{alt_src_count} times prior to each bandwidth measurement..."
+puts "Alternate source test type #{alt_src_type}..."
+puts "Requesting each URI #{request_count} time(s)..."
+puts "Accessing URI's listed in #{uri_filename}..."
+puts
 
 UNSET="<unavailable>"
 ERROR="<error>"
@@ -142,6 +173,7 @@ while (uri = uri_file_handle.gets)
         row.remote_geoip_info = UNSET
 
         row.timestamp = Time.now
+        sendAltSrcSyns(alt_src_count,uri,alt_src_type)
         begin
             row.bandwidth = measureWgetBdwth(uri)
         rescue
