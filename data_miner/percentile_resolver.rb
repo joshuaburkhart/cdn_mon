fc83cad6eab8f84243fa5e75e5a0f0f1cc56341d
Joshua Burkhart <burkhart.joshua@gmail.com>
2012-11-13 09:17:46 -0800

    pushing percentile calculation..next step test on pl nodes!

---
+++ data_miner/percentile_resolver.rb
@@ -0,0 +1,79 @@
+#!/usr/local/bin/ruby
+
+require 'optparse'
+
+options = {}
+optparse = OptionParser.new { |opts|
+    opts.banner = <<-EOS
+Usage: ruby percentile_resolver.rb -p <percentile limit> -f <list of common metrics> -o <output file>
+
+Example: ruby percentile_resolver.rb -p 75 -f ../test/average_rtts.csv -o ../test/top75rtts.csv
+
+Output Format:
+geoip 1
+.
+.
+geoip N
+    EOS
+    opts.on('-h','--help','Display this screen'){
+        puts opts
+        exit
+    }
+    options[:percentile] = 50
+    opts.on('-p','--percentile PERCENTILE','Percentile lower threshold PERCENTILE'){ |percentile|
+        options[:percentile] = Integer(percentile)
+    }
+    options[:infile] = nil
+    opts.on('-f','--file FILE','File Path With Metrics FILE'){ |file|
+        options[:infile] = file
+        options[:outfile] = "#{file}_top#{options[:percentile]}percentile"
+    }
+    opts.on('-o','--out FILE','Output File Path FILE'){ |file|
+        options[:outfile] = file
+    }
+}
+optparse.parse!
+if(options[:infile].nil?)
+    raise OptionParser::MissingArgument,"Metrics File Path = #{options[:infile]}"
+end
+
+puts "Percentile:  #{options[:percentile]}"
+puts "Input File:  #{options[:infile]}"
+puts "Output File: #{options[:outfile]}"
+
+out_handle = File.open(options[:outfile],'w')
+in_handle = File.new(options[:infile])
+
+orig_files = in_handle.gets #on first line
+puts orig_files
+geo_info = in_handle.gets #on second line
+puts geo_info
+geo_ary = geo_info.split
+nodes = Array.new
+(0..geo_ary.length - 1).each { |i|
+    nodes[i] = Array.new
+    nodes[i][0] = geo_ary[i]
+}
+
+while line = in_handle.gets
+    metrics_list = line.split
+    (1..metrics_list.length - 1).each { |i|
+        nodes[i][1] += metrics_list[i]
+    }
+end
+
+nodes.sort! { |i,j|
+    i[1] <=> j[1]
+}
+
+percentage = 1 - (options[:percentile] / 100.0)
+idx_threshold = percentage * nodes.length
+
+valid_idx = 0
+while valid_idx < idx_threshold
+    out_handle.puts(nodes[valid_idx])
+    valid_idx += 1
+end
+
+out_handle.close
+in_handle.close
--- data_miner/raw_stats.rb
+++ data_miner/raw_stats.rb
@@ -5,12 +5,14 @@ require 'optparse'
 options = {}
 optparse = OptionParser.new { |opts|
     opts.banner = <<-EOS
-Usage: ruby raw_stats.rb -f /cleaned/csv/1 [/cleaned/csv/2 ...] [-o /out/csv]
+Usage: ruby raw_stats.rb -l /cleaned/csv/1 [/cleaned/csv/2 ...] [-o /out/csv]
 
-Example 1: ruby raw_stats.rb -f ../test/out.geoip.csv
-Example 2: ruby raw_stats.rb -f ../test/out.geoip.csv ../test2/out.geoip.csv -o ../test/average_rtts.csv
+Example 1: ruby raw_stats.rb -l ../test/out.geoip.csv
+Example 2: ruby raw_stats.rb -l ../test/out.geoip.csv,../test2/out.geoip.csv -o ../test/average_rtts.csv
 
 Output Format:
+<first row has infile names>
+<second row has infile node geo info>
 uri,RTT avg 1,RTT avg 2,RTT avg N
     EOS
     opts.on('-h','--help','Display this screen') {
@@ -21,7 +23,7 @@ uri,RTT avg 1,RTT avg 2,RTT avg N
     opts.on('-l','--list a,b,c',Array,"Data File Paths - 'list' CSVs") { |l|
         options[:data_files] = l
     }
-    options[:out_file_path] = "#{options[:data_files]}.stats.csv"
+    options[:out_file_path] = "avg_rtt_stats.csv"
     opts.on('-o','--out FILE','Output File Path FILE') { |file|
         options[:out_file_path] = file
     }
@@ -72,9 +74,11 @@ out_file_handle.puts
 
 print "working..."
 REF_POS = 0
+GEO_POS = 3
 RTT_POS = 12
 URI_POS = 13
 CLB = "CACHE_LINE_BOOTSTRAP"
+FIRST_LINE = true
 cached_ref_uri = CLB
 while ref_line = data_handles[REF_POS].gets
     print "."
@@ -93,12 +97,18 @@ while ref_line = data_handles[REF_POS].gets
         }
         cached_ref_uri = ref_match
     end
+    if(FIRST_LINE)
+        out_file_handle.print("#{ref_line.split(',')[GEO_POS]},")  
+    end
     node_buffers[REF_POS] << ref_line.split(',')[RTT_POS]
     (1..data_handles.length - 1).each { |i|
         cpd_line = data_handles[i].gets
         cpd_line.split(',')[URI_POS].match(/(^http:\/\/[a-zA-Z0-9\.\-]+[a-z0-9][\/]*[a-zA-Z0-9\-\_\.\~\/]*|^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$)/)
         cpd_match = $1
         if(ref_match == cpd_match)
+            if(FIRST_LINE)
+                out_file_handle.print(cpd_line.split(',')[GEO_POS])
+            end
             node_buffers[i] << cpd_line.split(',')[RTT_POS]
         else
             puts "ERROR DETECTED, FOLLOWING LINES SHOULD MATCH:"
@@ -109,5 +119,9 @@ while ref_line = data_handles[REF_POS].gets
             exit 1
         end
     }
+    if(FIRST_LINE)
+        out_file_handle.print("\n")
+        FIRST_LINE = false
+    end
 end
 puts "done."
